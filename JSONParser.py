9ebe7b9e60429f5e880f2306fdd17264e26d6d26
dkennedy778 <derek.kennedy.13@cnu.edu>
2018-01-18 16:23:07 -0500

    Fixed some issues with date parsing
    Date parsing still not working because the parsed tweet's date isn't formatted like a normal twitter timestamp. Need to make a custom solution for the twitter parser's funky formatting.

--- JSONParser.py
+++ JSONParser.py
@@ -4,6 +4,7 @@ from urlextract import URLExtract
 from pprint import pprint
 import datetime
 import logging
+import time
 import collections
 
 tweetAndTime = collections.namedtuple('tweet','dateTime')
@@ -20,15 +21,21 @@ def parseTweets(filename):
     times = []
     #Want to sort the tweets with datetime objects, so here I'm just parsing through all of the tweets and converting their timestamps to datetime objects. I then store all tweets with their datetime objects into a tuple, which I put into a list of tuples.
     #This'll make sorting easier, but imposes an immediate O(N) performance cost, which may be significant with large tweet batches.
+
+    #Oh fuck are timezones going to fuck me? Great
     try:
         for dataPoint in data:
             Strtime = dataPoint["timestamp"]
-            time = datetime.datetime.strptime(Strtime,'%Y-%m-%dT%H:%M:%S.%fZ').time()
-            tweetToBeStored = tweetAndTime(dataPoint,time)
+            tweetTime = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(dataPoint['timestamp'],'%a %b %d %H:%M:%S +0000 %Y'))
+            tweetToBeStored = tweetAndTime(dataPoint,tweetTime)
             times.append(tweetToBeStored)
         #Now we have all of our tweets stored, lets sort them by their datetimes
         times.sort(key=lambda tup: tup[1])
 
-    #Next task is to split up tweets into blocks of ten minutes based on their date time 
+    #Next task is to split up tweets into blocks of ten minutes based on their date time
+    #First get the initial time
+        startTweet = times[0]
+        startTime = startTweet[1]
+
     except Exception as e:
         JSONParser_logger.exception("exception hit")
