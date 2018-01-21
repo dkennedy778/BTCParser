4664bdfe57c6407974837c2e7b8fc9cf166a86fa
dkennedy778 <derek.kennedy.13@cnu.edu>
2018-01-20 20:04:49 -0500

    Method now passes list of scores back to master

--- sentimentAnalyzer.py
+++ sentimentAnalyzer.py
@@ -4,7 +4,7 @@ import math
 #More than 50% of the tweets are junk, can't derive any sentiment information from them.
 def get_tweet_sentiment(tweets):
     scores = []
-    i = 0
+    validTweetsPerBlock = 0
     for block in tweets:
         blockScore = 0;
         for tweet in block:
@@ -13,14 +13,18 @@ def get_tweet_sentiment(tweets):
             #Testing stuff
             #print(sentiment.polarity)
             if(sentiment.polarity != 0):
-                i = i + 1 #incrementing the number of valid tweets in the block
+                validTweetsPerBlock = validTweetsPerBlock + 1 #incrementing the number of valid tweets in the block
                 #weight the score, weighting scale in ln()
                 if(int(tweet[0]["retweets"]) > 0):
                     score = sentiment.polarity + math.log(int(tweet[0]["retweets"]), 2.71828)
+                    #blockScore = blockScore + score
                 else:
                     score = sentiment.polarity
+                    #blockScore = blockScore + score
                 blockScore = blockScore + score
-        blockScore = blockScore/i
-        i = 0
+        blockScore = blockScore/validTweetsPerBlock
+        validTweetsPerBlock = 0
         print("BLOCK COMPLETE")
         print(blockScore)
+        scores.append(blockScore)
+    return scores
