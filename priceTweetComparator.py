f9abe51043ffc24c5425d29b50ba018bf719a3b9
dkennedy778 <derek.kennedy.13@cnu.edu>
2018-01-22 14:25:21 -0500

    Initial Comparator commit
    Hardest part of comparing these data sets is going to be matching up the time periods. Considering reworking the project flow around this issue. Prices would be parsed first, and from that information we would derive the intervals used to sort the tweets into blocks. Once we have those intervals, we can start parsing tweets from the first price point.

---
+++ priceTweetComparator.py
@@ -0,0 +1,26 @@
+def comparePricesandTwitter(marketInfo, sentiments):
+
+    #Get % change for prices over period
+    initialPrice = marketInfo[3][1] #USD price changes are the last list in the master list, prices are the first index
+    priceMovements = []
+    for prices in marketInfo[3]:
+        priceMovements.append(get_change(initialPrice,prices[1]))
+        initialPrice = prices[1]
+
+    #Get % change for twitter over period
+    sentimentMovements = []
+    initialSentiment = sentiments[0]
+    for sentiment in sentiments:
+        sentimentMovements.append(get_change(initialSentiment,sentiment[0]))
+        initialSentiment = sentiments[0]
+
+    #Compare the two lists
+    #May be worth ordering the sentiment blocks by the marketplace time periods given by coincap
+
+def get_change(current, previous):
+    if current == previous:
+        return 100.0
+    try:
+       return ((abs(current - previous))/previous)*100.0
+    except ZeroDivisionError:
+        return 0
