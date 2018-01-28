5f2d3786a9e83a559a136a6e30b8bf997e98348b
dkennedy778 <derek.kennedy.13@cnu.edu>
2018-01-28 15:04:22 -0500

    Initial commit for the Powershell scraper
    The idea behind this scraper is to automate the running of tweet collection on a separate machine. This script is not complete, it will currently run the twitterscraper searches concurrently. This is untenable given the large sizes of our tweet batches.

---
+++ twitterScraperScript.ps1
@@ -0,0 +1,30 @@
+Set-Location -Path "D:/Users/Derek/PostGrad/BTCParser"
+$startDate = Get-Date "1/12/2018"
+$currentDate = Get-Date
+$temp = $currentDate.ToShortDateString
+
+$ts = New-TimeSpan -Start $startDate -End $currentDate
+$DayCount = $ts.Days
+$DayCount
+$parseDate = $startDate
+For ($i = 0; $i -lt $DayCount; $i++){ #I miss you already python
+    $twitterScraperString = $parseDate + "BTCtweets.json"
+    twitterscraper "Bitcoin OR BTC " -l 2000000 -o =$twitterScraperString
+    $parseDate = $parseDate.AddDays(1)
+    $twitterScraperString = $parseDate + "BTCtweets.json"
+    twitterscraper "Bitcoin OR BTC " -l 2000000 -o =$twitterScraperString
+    $parseDate = $parseDate.AddDays(1)
+    $twitterScraperString = $parseDate + "BTCtweets.json"
+    twitterscraper "Bitcoin OR BTC " -l 2000000 -o =$twitterScraperString
+    $parseDate = $parseDate.AddDays(1)
+    $twitterScraperString = $parseDate + "BTCtweets.json"
+    twitterscraper "Bitcoin OR BTC " -l 2000000 -o =$twitterScraperString
+    $parseDate = $parseDate.AddDays(1)
+    $twitterScraperString = $parseDate + "BTCtweets.json"
+    twitterscraper "Bitcoin OR BTC " -l 2000000 -o =$twitterScraperString
+    $parseDate = $parseDate.AddDays(1)
+    $twitterScraperString = $parseDate + "BTCtweets.json"
+    twitterscraper "Bitcoin OR BTC " -l 2000000 -o =$twitterScraperString
+    $parseDate = $parseDate.AddDays(1)
+    $parseDate.Date 
+}
