7517d4b20bd29df5452222c99a681288a04eae78
dkennedy778 <derek.kennedy.13@cnu.edu>
2018-01-29 09:48:08 -0500

    Implemented experimental parallel scraping workflow
    Committing this to save the code, solution is proving untenable

--- twitterScraperScript.ps1
+++ twitterScraperScript.ps1
@@ -1,30 +1,37 @@
-Set-Location -Path "Your_Path"
+Set-Location -Path "D:/Users/Derek/PostGrad/BTCParser"
 $startDate = Get-Date "1/12/2018"
 $currentDate = Get-Date
 $temp = $currentDate.ToShortDateString
 
 $ts = New-TimeSpan -Start $startDate -End $currentDate
 $DayCount = $ts.Days
-$DayCount
 $parseDate = $startDate
+$dateList = New-Object System.Collections.ArrayList($null)
 For ($i = 0; $i -lt $DayCount; $i++){ #I miss you already python
-    $twitterScraperString = $parseDate + "BTCtweets.json"
-    twitterscraper "Bitcoin OR BTC " -l 2000000 -o =$twitterScraperString
+    $dateList.Add($parseDate)
     $parseDate = $parseDate.AddDays(1)
-    $twitterScraperString = $parseDate + "BTCtweets.json"
-    twitterscraper "Bitcoin OR BTC " -l 2000000 -o =$twitterScraperString
-    $parseDate = $parseDate.AddDays(1)
-    $twitterScraperString = $parseDate + "BTCtweets.json"
-    twitterscraper "Bitcoin OR BTC " -l 2000000 -o =$twitterScraperString
-    $parseDate = $parseDate.AddDays(1)
-    $twitterScraperString = $parseDate + "BTCtweets.json"
-    twitterscraper "Bitcoin OR BTC " -l 2000000 -o =$twitterScraperString
-    $parseDate = $parseDate.AddDays(1)
-    $twitterScraperString = $parseDate + "BTCtweets.json"
-    twitterscraper "Bitcoin OR BTC " -l 2000000 -o =$twitterScraperString
-    $parseDate = $parseDate.AddDays(1)
-    $twitterScraperString = $parseDate + "BTCtweets.json"
-    twitterscraper "Bitcoin OR BTC " -l 2000000 -o =$twitterScraperString
-    $parseDate = $parseDate.AddDays(1)
-    $parseDate.Date 
 }
+$DayCount
+scraper($dateList)
+
+
+Workflow scraper{
+
+    param(
+    [Parameter (Mandatory = $true)]
+    [System.Collections.ArrayList]$dateList
+  )
+
+        #I used those ugly ifs to avoid a situation where the script was trying to parse a date in the future (i.e. outside the boundaries of the array).
+        #Please PM me if you have a more elegant solution 
+        ForEach -Parallel ($date in $dateList)
+        {      
+              $endDate = $date.AddDays(1)
+              $startDateString = $date.ToString("yyyy-MM-dd")
+              $endDateString = $endDate.ToString("yyyy-MM-dd")
+              $twitterScraperString = $startDateString + "BTCtweets.json"
+              
+              twitterscraper "Bitcoin OR BTC " -begindate $startDateString -enddate $endDateString --"limit" 20 -o =$twitterScraperString  
+        }
+
+    }
